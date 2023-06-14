cca0339b62142d1d34422c06da6da3da88c4aea5
elsawanjiru <elsa.wanjiru@student.moringaschool.com>
2023-06-14 14:07:11 +0300

    completd assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!';
+
+const firstInitial = currentUser[0];
+const shortGreeting = 'Welcome, ' + firstInitial + '!';
+
+
+
+module.exports = {
+  currentUser,
+  welcomeMessage,
+  excitedWelcomeMessage,
+  shortGreeting
+};
